d4b0a54399914f831bb6ac37ba8f29360b7e8746
Aashika-Pandey <aashika100pandey@gmail.com>
2026-05-06 10:40:44 +0545

    Add files via upload

---
+++ Firstprogram.py
@@ -0,0 +1,125 @@
+# #--> variable is a name give to a memory location in a program
+# name = "Aashika"
+# age = 22
+# price = 28.8
+
+# age2=age 
+
+# print("my name is:", name)
+# print("my age is:", age)
+# print("the price is:", price)
+# print(age2)
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# # rules for identifier -uppercase, lowercase, digits , _ etc
+
+# Data Types-integer, string, float, boolean, none--primary data types
+# name1 ='sk'
+# name2 ="sk"
+# name3 ='''sk'''
+# print(name1)
+# print(name2)
+# print(name3)
+
+# age = 23
+# old = False
+# a = None
+# print(type(age))
+# print(type(old))
+# print(type(a))
+
+#keywords--> these are the reserved words in python
+
+"""sum of two number 
+ a = 2
+ b = 5
+ sum = a + b
+ print(sum)"""
+
+# types of operator --> arithmetic , relational/comparision, assignment, logical
+
+# 1. Arithmetic Operator
+#sum of two number 
+"""a = 2
+b = 5
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)"""
+
+# 2. Relational operator
+# a = 50
+# b = 20
+
+# print(a == b)
+# print(a != b)
+# print(a >= b)
+# print(a > b)
+# print(a <= b)
+# print(a < b)
+
+# 3. Assignment operator
+# num = 10
+# num = num + 10
+# num += 10
+# print(num)
+
+# 4. Logical Operator
+"""a = 50
+b = 30
+print(a > b)
+print(not (a > b))"""
+
+"""val1 = True
+val2 = True
+print("and opertor:", val1 and val2)"""
+
+"""val1 = True
+val2 = False
+print("or opertor:", val1 or val2)"""
+
+#Type conversion and Type Casting
+
+"""a = 2    int to float
+b = 4.25
+sum = a + b
+print(sum)"""
+
+"""a = "5"   type casting
+b = int(a)
+c = 10
+print(b + c)"""
+
+"""name = input("enter your name: ")
+print("welcome ", name)"""
+"""val = input("enter some value : ")
+print(type(val), val)"""
+"""int("5")
+val = int(input("enter some value : "))
+print(type(val), val)"""
+
+"""name = input("enter name :")
+age = input("enter age:")
+marks = input("enter marls: ")
+print("welcome ", name)
+print("age =", age)
+print("marks =", marks)"""
+
+"""a = int(input("enter 1st num :"))
+b = int(input("enter 2nd num :"))
+print("sum =", a + b)"""
+
+"""a = int(input("enter side of square :"))
+print("area =", a * a)"""
+
+"""a = int(input("enter first :"))
+b = int(input("enter second :"))
+print("avg =", (a + b) / 2)"""
+
+"""a = int(input("enter first :"))
+b = int(input("enter second :"))
+print(a >= b)"""
